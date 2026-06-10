4a894a302230c4ef74b1242b4db33642e420f4f8
Lukas Burgstaller <mail@voidstern.net>
2026-06-10 09:45:50 +0200

    Fixed for Xcode 27

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     platforms: [
         .macOS(.v12),
         .iOS(.v15),
+        .watchOS(.v10),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
